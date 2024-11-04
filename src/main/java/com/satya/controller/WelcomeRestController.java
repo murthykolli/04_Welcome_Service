b797736d6ce5a4eab2247b5298818008dfa10688
murthy.kolli@gmail.com <murthy.kolli@gmail.com>
2024-11-04 12:38:04 +0530

    Welcome Service commited

---
+++ src/main/java/com/satya/controller/WelcomeRestController.java
@@ -0,0 +1,21 @@
+package com.satya.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeRestController {
+	
+	@Autowired
+	private Environment env;
+	
+	@GetMapping("/welcome")
+	public String getWelcomeMsg() {
+		String port = env.getProperty("server.port");
+		String msg = "Welcome to Hyderabad...." + port;
+		return msg;
+	}
+
+}
